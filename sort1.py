ba43e8ca26d5c5887da667bd26fea7dcd7393130
NorthCynosure <674164335@qq.com>
2018-10-19 02:57:32 +0800

    python sort

---
+++ sort1.py
@@ -0,0 +1,29 @@
+def bubble_sort(alist):
+    for i in range (len(alist) - 1):
+    	swapped = False
+    	for j in range(len(alist) - 1 - i):
+    		if alist[j] > alist[j + 1]:
+    			alist[j], alist[j + 1] = alist[j + 1], alist[j]
+    			swapped = True
+    	if swapped:
+    		swapped = False
+    		for j in range(len(alist) - 2 - i,0,-1):
+    			
+
+
+class Student(object):
+    def __init__(self,name,classes):
+        self.name = name
+        self.classes = classes
+
+    def __gt__(self,other):
+    	return self.classes > other.classes
+
+    def __lt__(self,other):
+    	return self.classes < other.classes
+
+    def __reper__(self):
+    	return f'{self.name}:{self.age}'
+
+
+
